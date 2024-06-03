ee8a0dee8e9b4e39e04d7b0db44ffa57f8348a02
kambam-sathwik <sathwikkambam@gmail.com>
2024-06-03 09:01:52 +0530

    created server

--- first-app.js
+++ first-app.js
@@ -1,2 +1,7 @@
-const fs=require('fs');
-fs.writeFileSync('hi.txt','aygsfdyttdsffy');
+// const fs=require('fs');
+// fs.writeFileSync('hi.txt','aygsfdyttdsffy');
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    console.log("sathwik")
+})
+server.listen(4000);
