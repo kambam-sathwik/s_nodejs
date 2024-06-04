6a5c9a4a1d86152324dfba228fbd96c830c2a89b
kambam-sathwik <sathwikkambam@gmail.com>
2024-06-04 08:27:15 +0530

    worked with req and res

--- first-app.js
+++ first-app.js
@@ -2,6 +2,12 @@
 // fs.writeFileSync('hi.txt','aygsfdyttdsffy');
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log("sathwik")
+    // console.log("sathwik")
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My page</title></title></head>');
+    res.write('<body><h1>My first node js program</h1></body>')
+    res.write('</html>');
+    res.end();
 })
 server.listen(4000);
